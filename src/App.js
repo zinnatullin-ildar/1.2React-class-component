21e1fad5239239bb762905543ea0a07bc28a33af
Ildar Zinnatullin <iqz@izh.com>
2022-11-21 22:44:32 +0400

    Fixed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,5 @@
 import ShopItemClass from './components/ShopItemClass';
-
-const item = {
-    brand: 'Tiger of Sweden',
-    title: 'Leonard coat',
-    description: 'Minimalistic coat in cotton-blend',
-    descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
-    price: 399,
-    currency: '£'
-}
+import item from './data/Data';
 
 function App() {
     return (
--- src/components/ShopItemClass.jsx
+++ src/components/ShopItemClass.jsx
@@ -2,31 +2,35 @@ import React from "react";
 import PropTypes from 'prop-types';
 
 class ShopItemClass extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+    // constructor(props) {
+    //     super(props);
+    // }
 
     render() {
         const { item } = this.props;
+
         return (
             <div className="main-content">
                 <h2>{item.brand}</h2>
                 <h1>{item.title}</h1>
                 <h3>{item.description}</h3>
-                <div className="description">{item.descriptionFull}</div>
-                <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+                <div className="description">
+                    {item.descriptionFull}
+                </div>
+                <div className="highlight-window mobile">
+                    <div className="highlight-overlay"></div>
+                </div>
                 <div className="divider"></div>
                 <div className="purchase-info">
-                    <div className="price">{`${item.currency}${item.price}.00`}</div>
+                    <div className="price">
+                        {`${item.currency}${item.price}.00`}
+                    </div>
                     <button>Добавить в корзину</button>
                 </div>
             </div>
         );
-
     }
 }
-
-export default ShopItemClass;
 
 ShopItemClass.propTypes = {
     item: PropTypes.shape({
@@ -38,3 +42,5 @@ ShopItemClass.propTypes = {
         currency: PropTypes.string,
     }),
 };
+
+export default ShopItemClass;
